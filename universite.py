7c7a0c2ce8b758da9dd45a452ed9bf9798fb0a54
cocolaloutre <corentin.nsg.leroy@gmail.com>
2018-05-07 23:18:50 +0200

    CRUD + ajout fichier main pour db

--- universite.py
+++ universite.py
@@ -2,31 +2,37 @@ import sys
 import pymongo
 from pprint import pprint
 from bson.objectid import ObjectId
-
-from pymongo import MongoClient
-client = MongoClient('localhost', 27017)
-db = client.database
-universites = db.universites
+import db
 
 class Universite :
 
-    def __init__(self, univId, continent, pays, nom, departements) :
+    def __init__(self, univId, nom, pays, departements) :
         self.id = univId
-        self.continent = continent
-        self.pays = pays
         self.nom = nom
+        self.pays = pays
         self.departements = departements
+
+    def __str__(self) :
+        return "Université " + str(self.id) + " : " + self.nom + "(" + self.pays + "), disponible pour les départements " + str(self.departements)
+
+    # Met à jour les variables
+    def update(self, **kwargs) :
+        if kwargs.get('nom') :
+            self.nom = kwargs.get('nom')
+        if kwargs.get('pays') :
+            self.pays = kwargs.get('pays')
+        if kwargs.get('departements') :
+            self.departements = kwargs.get('departements')
 
     # Insère l'université dans la collection universites
     def insererDansCollection(self) :
-        if universites.find({"_id" : self.id}).count() > 0 :
+        if db.universites.find({"_id" : self.id}).count() > 0 :
             print("L'université " + str(self.id) + " existe déjà.", file=sys.stderr)
         else :
             univ = {
                 "_id" : self.id,
-                "continent" : self.continent,
-                "pays" : self.pays,
                 "nom" : self.nom,
+                "pays" : self.pays,
                 "departements" : self.departements,
             }
-            universites.insert_one(univ)
+            db.universites.insert_one(univ)
--- utilisateur.py
+++ utilisateur.py
@@ -2,15 +2,11 @@ import sys
 import pymongo
 from pprint import pprint
 from bson.objectid import ObjectId
-
-from pymongo import MongoClient
-client = MongoClient('localhost', 27017)
-db = client.database
-utilisateurs = db.utilisateurs
+import db
 
 class Utilisateur :
 
-    def __init__(self, uId, nom, prenom, departement, niveau, mobilite, mail) :
+    def __init__(self, uId, prenom, nom, departement, niveau, mobilite, mail) :
         self.id = uId
         self.nom = nom
         self.prenom = prenom
@@ -19,9 +15,27 @@ class Utilisateur :
         self.mobilite = mobilite
         self.mail = mail
 
+    def __str__(self) :
+        return "Utilisateur " + str(self.id) + " : " + self.prenom + " " + self.nom + ", " + str(self.niveau) + self.departement + ", mobilite : " + str(self.mobilite) + ", " + self.mail
+
+    # Met à jour les variables
+    def update(self, **kwargs) :
+        if kwargs.get('prenom') :
+            self.prenom = kwargs.get('prenom')
+        if kwargs.get('nom') :
+            self.nom = kwargs.get('nom')
+        if kwargs.get('departement') :
+            self.departement = kwargs.get('departement')
+        if kwargs.get('niveau') :
+            self.niveau = kwargs.get('niveau')
+        if kwargs.get('mobilite') :
+            self.mobilite = kwargs.get('mobilite')
+        if kwargs.get('mail') :
+            self.mail = kwargs.get('mail')
+
     # Insère l'utilisateur dans la collection utilisateurs
     def insererDansCollection(self) :
-        if utilisateurs.find({"_id" : self.id}).count() > 0 :
+        if db.utilisateurs.find({"_id" : self.id}).count() > 0 :
             print("L'utilisateur " + str(self.id) + " existe déjà.", file=sys.stderr)
         else :
             utilisateur = {
@@ -33,4 +47,4 @@ class Utilisateur :
                 "mobilite" : self.mobilite,
                 "mail" : self.mail
             }
-            utilisateurs.insert_one(utilisateur)
+            db.utilisateurs.insert_one(utilisateur)
