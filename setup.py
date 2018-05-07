02812a50235a33d91970199d118857645ffd882b
GregoireZytko <antoinebroquet01@gmail.com>
2018-05-07 18:33:13 +0200

    Fix setup.py

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ setup(
     include_package_data=True,
     zip_safe=False,
     install_requires=[
-        'Flask=1.0.2',
+        'Flask==1.0.2',
         'Flask-Mail',
         'Flask-Flatpages',
         'Flask-Login',
