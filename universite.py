201475d5053f795b81793c03bddca8ac5b7d2e61
cocolaloutre <corentin.nsg.leroy@gmail.com>
2018-05-07 17:55:04 +0200

    Classes pour les documents des différentes collections

---
+++ universite.py
@@ -0,0 +1,32 @@
+import sys
+import pymongo
+from pprint import pprint
+from bson.objectid import ObjectId
+
+from pymongo import MongoClient
+client = MongoClient('localhost', 27017)
+db = client.database
+universites = db.universites
+
+class Universite :
+
+    def __init__(self, univId, continent, pays, nom, departements) :
+        self.id = univId
+        self.continent = continent
+        self.pays = pays
+        self.nom = nom
+        self.departements = departements
+
+    # Insère une université dans la collection universites
+    def insererDansCollection(self) :
+        if universites.find({"_id" : self.id}).count() > 0 :
+            print("L'université " + str(self.id) + " existe déjà.", file=sys.stderr)
+        else :
+            univ = {
+                "_id" : self.id,
+                "continent" : self.continent,
+                "pays" : self.pays,
+                "nom" : self.nom,
+                "departements" : self.departements,
+            }
+            universites.insert_one(univ)
