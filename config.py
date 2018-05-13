d050e46feae50ced06c5fe3c69ce76457cfad0ca
GregoireZytko <antoinebroquet01@gmail.com>
2018-05-13 10:36:05 +0200

    Corrrection config.py

--- config.py
+++ config.py
@@ -1,20 +1,20 @@
 import os
 
-class Config(object):
-    
+
+
     # Security
-    DEBUG = True
-    SECRET_KEY = 'ngjqVmXKmqfKN46jKIzj'
-    ROOT = "superuser"
-    ROOT_PSWD = "yoMama"
+DEBUG = True
+SECRET_KEY = 'ngjqVmXKmqfKN46jKIzj'
+ROOT = "superuser"
+ROOT_PSWD = "yoMama"
 
     # Mail
-    MAIL_SERVER = 'smtp.gmail.com'
-    MAIL_PORT = 465
-    MAIL_USE_SSL = True
-    MAIL_USERNAME = 'dont.reply.to.project.whiskey'
-    MAIL_PASSWORD = 'webWEB123'
-    MAIL_DEFAULT_SENDER = 'Project Whiskey <dont.reply.to.project.whiskey@gmail.com>'
+MAIL_SERVER = 'smtp.gmail.com'
+MAIL_PORT = 465
+MAIL_USE_SSL = True
+MAIL_USERNAME = 'dont.reply.to.project.whiskey'
+MAIL_PASSWORD = 'webWEB123'
+MAIL_DEFAULT_SENDER = 'Project Whiskey <dont.reply.to.project.whiskey@gmail.com>'
 
-    # Auth
-    SESSION_COOKIE_NAME = 'session'
+# Auth
+SESSION_COOKIE_NAME = 'session'
